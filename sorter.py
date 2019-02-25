17d43ddec59e520f6bc2ec52ce6cf4697c167123
Ramna Sidharta <ramna.thors@gmail.com>
2019-02-24 22:02:07 -0300

    Applying Strategy pattern

--- sorter.py
+++ sorter.py
@@ -1,15 +1,94 @@
-class Sorter(object):
-    _rules = None
+import abc
 
+class BookReceptor(object):
+    
+    def __init__(self, strategy, cfgfilename=None):
+        self._sortstrategy = strategy
+        if cfgfilename:
+            conf_file = open(self._cfgfilename, 'r')
+            self._rules = [raw[:-1] for raw in conf_file.readlines()]
+
+    def sort(self):
+        return self._sortstrategy.sort()
+
+class BookSorter(abc.ABC):
+    @abc.abstractmethod
+    def sort(self, reverse=False):
+        pass
+
+
+class TitleSorter(BookSorter):
+    
+    def __init__(self, books):
+        self._books = {}
+        for b in books:
+            self._books[b.title.lower()] = b
+
+    def sort(self, reverse=False):
+        titles = list(map(lambda x: x.title.lower(), self._books.values()))
+        sorted_titles = sorted(titles, reverse=reverse)
+        return [self._books[title] for title in sorted_titles]
+
+
+class AuthorSorter(BookSorter):
+
+    def __init__(self, books):
+        self._books = {}
+        for b in books:
+            self._books[b.author.lower()] = b
+
+    def sort(self, reverse=False):
+        authors = list(map(lambda x: x.title.lower(), self._books.values()))
+        sorted_authors = sorted(authors, reverse=reverse)
+        return [self._books[title] for title in sorted_authors]
+
+
+class EditionYearSorter(BookSorter):
+
+    def __init__(self, books):
+        self._books = {}
+        for b in books:
+            self._books[b.edition_year.lower()] = b
+
+    def sort(self, reverse=False):
+        edyear = list(map(lambda x: x.title.lower(), self._books.values()))
+        sorted_edyear = sorted(edyear, reverse=reverse)
+        return [self._books[title] for title in sorted_edyear]
+
+    '''
     def __init__(self, rules=None):
-        if rules != None:
-            self._rules = rules
+        self._rules = rules
+        self.sorting_types = {\
+            "__sortbytitle": self.__sortbytitle,\
+            "__sortbyauthor": self.__sortbyauthor,\
+            "__sortbyedition": self.__sortbyedition\
+        }
 
     def sort(self, books):
         if self._rules == None:
             return self.__sortbytitle(books)
 
-    def __sortbytitle(self, books):
+        sorted_books = []
+        for rule_raw in self._rules:
+            rule = rule_raw.split(' ')
+            if len(rule) == 2:
+                print(rule[0], ' ', rule[1])
+                sorting_types[rule[0]](books, rule[1] == '1')
+            else:
+                self.sorting_types["__" + rule[0]](books)
+
+    def __sortbytitle(self, books, reverse=False):
+        titles = list(map(lambda x: x.title.lower(), books.values()))
+        sorted_titles = sorted(titles, reverse=reverse)
+        return [books[title] for title in sorted_titles]
+
+    def __sortbyauthor(self, books, reverse=False):
+        authors = list(map(lambda x: x.author.lower(), books.values()))
+        sorted_authors = sorted(authors)
+        return [books[title] for title in sorted_authors]
+
+    def __sortbyedition(self, books, reverse=False):
         titles = list(map(lambda x: x.title.lower(), books.values()))
         sorted_titles = sorted(titles)
         return [books[title] for title in sorted_titles]
+        '''
--- sorting_service.py
+++ sorting_service.py
@@ -1,50 +1,18 @@
 #!/usr/bin/python
 
 from book import Book
-from sorter import Sorter
+from sorter import BookReceptor, TitleSorter
 import sys
 
-class BookReceptor(object):
+def booklist():
+    books_file = open("books.txt", 'r')
+    books_raw = [raw[:-1] for raw in books_file.readlines()]
+    books = []
+    for bstr in books_raw:
+        infos = bstr.split(',')
+        books.append(Book(infos[0], infos[1], infos[2]))
 
-    __instance = None
-
-    @staticmethod
-    def getInstance():
-        if BookReceptor.__instance == None:
-            BookReceptor()
-        return BookReceptor.__instance
-    
-
-    def __init__(self):
-        if BookReceptor.__instance != None:
-            raise Exception("This class cannot be instantiated directly!")
-        else:
-            BookReceptor.__instance = self
-
-
-    def sort_books(self, rules=None):
-        books_file = open("books.txt", 'r')
-        books_raw = [raw[:-1] for raw in books_file.readlines()]
-        books = self.__books_dict(books_raw)
-
-        sorter = Sorter(rules)
-        return sorter.sort(books)
-    
-
-    def sorting_rules(conf_file_name):
-        conf_file = open(conf_file_name, 'r')
-        rules_raw = [raw for raw in conf_file.readlines()]
-        return rules_raw.split()
-
-
-    def __books_dict(self, books_raw):
-        books = {}
-        for book_str in books_raw:
-            infos = book_str.split(',')
-            title = infos[0]
-            books[title.lower()] = Book(title, author=infos[1], edition_year=infos[2])
-        return books
-
+    return books
 
 
 if __name__ == "__main__":
@@ -53,10 +21,11 @@ if __name__ == "__main__":
                 + "You should run 'python sorting_service.py <sort.conf>'.")
         exit(1)
 
-    book_receptor = BookReceptor()
     if len(sys.argv) == 1:
         print("No configuration file. Default sorting: Title ascending.")
-        sorted_books = book_receptor.sort_books()
+        books = booklist()
+        context = BookReceptor(TitleSorter(books))
+        sorted_books = context.sort()
     elif len(sys.argv) == 2:
         conf_file_name = sys.argv[1]
         rules = book_receptor.sorting_rules(conf_file_name)
