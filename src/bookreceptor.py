7f9237fdaf01b726c83322fadbd55cfb5e09e100
Ramna Sidharta <ramna.thors@gmail.com>
2019-02-26 12:38:48 -0300

    Organizing files in directories src/ and resources/
    Modification in booksreceptor.py: removing unecessary if, now that
    configuration file always will have two numbers per line

--- src/bookreceptor.py
+++ src/bookreceptor.py
@@ -15,15 +15,15 @@ class BookReceptor(object):
 
     def sort(self):
         if self._rulesraw is None:
-            '''Default sorting strategy, by title (TitleSorter)'''
+            '''Default sorting strategy, by title (strategy TitleSorter)'''
             return self._strategies[0].sort()
 
         sorted_books = []
         limits = [0, len(self._books)]
         while self._rulesraw:
             self._define_curr_rule(self._rulesraw.pop())
-            currstrategy, sorting_order = self._curr_rule
-            sorted_books = currstrategy.sort(reverse=sorting_order, start=limits[0], end=limits[1])
+            currstrategy, reverse = self._curr_rule
+            sorted_books = currstrategy.sort(reverse=reverse, start=limits[0], end=limits[1])
             limits = currstrategy.equal_elements()
             if limits == []:
                 break
@@ -32,12 +32,10 @@ class BookReceptor(object):
 
 
     def _define_curr_rule(self, rawrule):
-        sorting_order = 0
         r = rawrule.split(' ')
-        if len(r) == 2:
-            sorting_order = int(r[1])  == 1
         strategy_index = int(r[0]) - 1
-        self._curr_rule = [self._strategies[strategy_index], sorting_order]
+        reverse = int(r[1])  == 1
+        self._curr_rule = [self._strategies[strategy_index], reverse]
 
 
     def _sublist_books(self, limits):
