f2f512983a373af0dee807008ae3bd8acc719277
Ramna Sidharta <ramna.thors@gmail.com>
2019-02-26 15:06:34 -0300

    Rename BookReceptor -> BookReceiver

--- src/bookreceiver.py
+++ src/bookreceiver.py
@@ -1,10 +1,10 @@
 from sorter import TitleSorter, AuthorSorter, EditionYearSorter
 
-class BookReceptor(object):
+class BookReceiver(object):
 
     def __init__(self, books, cfgfilename=None):
-        self._books = books
-        self._strategies = [TitleSorter(books), AuthorSorter(books), EditionYearSorter(books)]
+        self._context = Context(books)
+        self._strategies = [TitleSorter(self._context), AuthorSorter(self._context), EditionYearSorter(self._context)]
         self._curr_rule = None
         self._rulesraw = None
         if cfgfilename is not None:
@@ -19,16 +19,15 @@ class BookReceptor(object):
         of books that should be sorted by the next BookSorter/strategy.'''
 
         if self._rulesraw is None:
-            '''Default sorting strategy, by title (strategy TitleSorter)'''
-            return self._strategies[0].sort()
+            return context.update(self._strategies[0]).sort()
 
         sorted_books = []
-        limits = [0, len(self._books)]
+        limits = [0, len(self._context.books)]
         while self._rulesraw:
             self._define_curr_rule(self._rulesraw.pop())
             currstrategy, reverse = self._curr_rule
-            sorted_books = currstrategy.sort(reverse=reverse, start=limits[0], end=limits[1])
-            limits = currstrategy.equal_elements()
+            sorted_books = self._context.update(currstrategy, limits=limits, reverse=reverse).sort()
+            limits = self._context.equal_elements_indexes()
             if limits == []:
                 break
 
@@ -43,3 +42,27 @@ class BookReceptor(object):
         self._curr_rule = [self._strategies[strategy_index], reverse]
 
 
+
+class Context(object):
+
+    def __init__(self, books, strategy=None, limits=None, reverse=False):
+        self.strategy = strategy
+        self.books = books
+        if limits is None:
+            self.start, self.end = 0, len(self.books)
+        self.reverse = reverse
+
+    def update(self, strategy, books=None, limits=None, reverse=False):
+        self.strategy = strategy
+        self.reverse = reverse
+        if books is not None:
+            self.books = books
+        if limits is not None:
+            self.start, self.end = limits
+        return self
+
+    def sort(self):
+        return self.strategy.sort(self)
+
+    def equal_elements_indexes(self):
+        return self.strategy.equal_elements()
