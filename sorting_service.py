f84e3dd97c405af426e00c52c2083c2712c2d32b
Ramna Sidharta <ramna.thors@gmail.com>
2019-02-26 12:08:44 -0300

    Changing program input interpretation
    The program will be run as `python sorting_service.py <booksfile>
    [OPTIONAL <configfile>]`.
    
    The "Null" entry will be interpreted as when no
    file of books are provided.
    The "empty" output will be shown when a file with no book was provided.

--- sorting_service.py
+++ sorting_service.py
@@ -4,36 +4,53 @@ from book import Book
 from bookreceptor import BookReceptor
 import sys
 
-def booklist():
-    books_file = open("books.txt", 'r')
+
+class OrderingException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+def booklist(filename):
+    books_file = open(filename, 'r')
     books_raw = [raw[:-1] for raw in books_file.readlines()]
     books = []
     for bstr in books_raw:
         infos = bstr.split(',')
-        books.append(Book(infos[0], infos[1], infos[2]))
-
+        try:
+            books.append(Book(infos[0], infos[1], infos[2]))
+        except:
+            raise OrderingException("Did you provide a file of a list of books?\nYou should run: "\
+                    + "python sorting_service.py <books.txt> [OPTIONAL <sort.conf>]")
     return books
 
+def printbooks(books):
+    if not books:
+        print("(Empty list)")
+    else:
+        last = books.pop()
+        for b in books:
+            print(b, '\n')
+        print(last)
 
 def main():
-    books = booklist()
-    if len(sys.argv) == 1:
-        print("No configuration file. Default sorting: Title ascending.")
+    numargs = len(sys.argv)
+
+    if numargs > 3:
+        print("Error! More than 2 argument were provided.\n"\
+                + "You should run 'python sorting_service.py <booksfile> [OPTIONAL <sort.conf>]'.")
+        exit(1)
+
+    books = booklist(sys.argv[1])
+    if numargs == 2:
+        print("No configuration file was provided. Default sorting: Title ascending.")
         context = BookReceptor(books)
         sorted_books = context.sort()
-    elif len(sys.argv) == 2:
-        context = BookReceptor(books, cfgfilename=sys.argv[1])
+    elif len(sys.argv) == 3:
+        context = BookReceptor(books, cfgfilename=sys.argv[2])
         sorted_books = context.sort()
     
-    for b in sorted_books:
-        print(b, '\n')
+    printbooks(sorted_books)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
-        print("Error! More than 1 argument were provided.\n"\
-                + "You should run 'python sorting_service.py <sort.conf>'.")
-        exit(1)
-
     main()
 
